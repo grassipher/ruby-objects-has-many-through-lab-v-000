3b76915146caf91e6a5b700bb36569f8493c52f2
Gabriel Valle <gvalle91@gmail.com>
2018-06-19 20:34:58 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -12,6 +12,18 @@ class Patient
     create_appointment = Appointment.new(date, self, doctor)
   end
   
+  def appointments
+    Appointments.all.select do |appointment_list|
+      appointment_list.patient == self
+    end
+  end
+  
+  def doctors
+    self.appointments.collect do |appointment_list|
+      appointment_list.doctor
+    end
+  end
+  
   def self.all
     @@all
   end
